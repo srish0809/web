acde8067ea5204339a494c585a0a1df5334310ad
srish0809 <srashtisoni2001.ss@gmail.com>
2022-11-24 12:11:49 +0530

    first commit

--- az.js
+++ az.js
@@ -1,1 +1,11 @@
-console.log("a");
+// var student={age:20};
+// var Student=function(){
+//     return this.age;
+// };
+// console.log(Student.bind(student));
+
+let student=function(age){
+    console.log(age);
+}
+let printage=student.bind(this,20);
+printage();
